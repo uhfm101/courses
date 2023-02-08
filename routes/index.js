137532bf49ab2fddda7c7c65862e7cdadd4170e1
uhfm <uhfm@fresnou.org>
2023-02-08 11:38:25 -0800

    start

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,12 @@ router.get('/courses', courseController.viewAll)
 
 router.get('/courses/profile/:id', courseController.viewProfile)
 
+router.get('/courses/edit/:id', courseController.renderEditForm)
+router.post('/courses/edit/:id', courseController.updateCourse)
+
+router.get('/courses/add', courseController.renderAddForm)
+router.post('/courses/add', courseController.addCourse)
+
+router.get('/courses/delete/:id', courseController.deleteCourse)
+
 module.exports = router;
